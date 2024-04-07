ed1efabcbe5ee62cb814f089c2a8b9436d95b895
caando <zhangjikun907475@gmail.com>
2024-04-08 05:43:36 +0800

    fix eslint and jest configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   env: {
     es2024: true,
     node: true,
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-	testMatch: ["**/tests/**/*.test.ts"],
-	transform: {
-		"\\.ts$": "@swc/jest",
-	},
+  preset: 'ts-jest',
+  testEnvironment: 'node',
 };
