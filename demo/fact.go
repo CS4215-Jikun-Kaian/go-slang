32a868b1c943f39ab239be3c7375e89e9f61e9d4
Cai <caipngg@gmail.com>
2024-04-20 02:32:20 +0800

    add demos

---
+++ demo/fact.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func factorial(n int) int {
+  if n == 1 {
+    return 1
+  } else {
+    return factorial(n - 1) * n
+  }
+}
+
+func main() {
+  fmt.Println(factorial(5))
+}
+
